5738302b1af9376e3aca7974ffcef0032318561b
estebanTMachine <estebanc@tmachine.com.uy>
2019-11-28 12:24:06 -0300

    InitialCommit

---
+++ monitor/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+//primera prueba de git con vcode-platformio-arduino
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
